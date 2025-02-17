6c15e1d41e8a174e05c06089a253d8b4e08cb2b0
meowcharli <169078644+meowcharli@users.noreply.github.com>
2025-02-17 19:44:04 +0100

    Update layout.tsx

--- app/(sanity)/layout.tsx
+++ app/(sanity)/layout.tsx
@@ -1,10 +1,11 @@
 import "../globals.css";
+import { Noto_Serif } from "next/font/google";
 
-import { Inter } from "next/font/google";
-
-const inter = Inter({
-  variable: "--font-inter",
+// Configure Noto Serif font
+const notoSerif = Noto_Serif({
+  variable: "--font-noto-serif",
   subsets: ["latin"],
+  weight: ["400", "700"], // Adjust weights as needed
   display: "swap",
 });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang="en" className={notoSerif.variable}>
       <body className="min-h-screen">{children}</body>
     </html>
   );
