e196ca55faf8a84687e8083abcbc8bdb7f8f34c8
meowcharli <169078644+meowcharli@users.noreply.github.com>
2025-02-17 23:37:32 +0100

    Update layout.tsx

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -79,8 +79,8 @@ export default async function RootLayout({
                 />
               ) : (
                 <div className="flex flex-col items-center py-28 lg:flex-row">
-                  <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
-                    Built with Next.js.
+                  <h3 className="mb-7 text-[#ddcdbf] text-center text-4xl leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
+                    Designed with ♥️ in Serbia.
                   </h3>
                   <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
                     <a
