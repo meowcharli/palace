c49e93d0dee7197844633515a18813335421166b
meowcharli <169078644+meowcharli@users.noreply.github.com>
2025-02-27 00:45:49 +0100

    Update layout.tsx

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -195,20 +195,20 @@ export default async function RootLayout({
             ) : (
               <div className="flex flex-col items-center py-12 lg:flex-row">
                 <h3 className="mb-6 text-[#828282] text-center text-1xl leading-tight tracking-tighter py-3 px-12 lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-1xl">
-                  Designed with ♥️ in Serbia.
+                  Designed with ♡ in Serbia.
                 </h3>
                 <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
                   <a
                     href="https://bsky.app/profile/4sigs.com"
                     className="mx-3 mb-6 border border-[#828282] bg-[#DDDDDD] rounded-full py-3 px-12 font-bold text-white transition-colors duration-200 hover:bg-transparent hover:text-[#183f63] lg:mb-0 lg:px-8"
                   >
-                    View our BlueSky 🦋
+                    View our BlueSky ➜
                   </a>
                   <a
                     href="mailto:hello@4sigs.com?subject=*Loud%20honk*%20This%20is%20me--%20contacting%20you!"
                     className="mx-3 mb-6 border border-[#828282] bg-[#DDDDDD] rounded-full py-3 px-12 font-bold text-[#828282] transition-colors duration-200 hover:bg-transparent hover:text-[#89131f] lg:mb-0 lg:px-8"
                   >
-                    Email us! 📧
+                    Email us! ➜
                   </a>
                 </div>
               </div>
