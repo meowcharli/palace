0bd56419c0f0895361409ee3aaf66b3d0646f33d
meowcharli <169078644+meowcharli@users.noreply.github.com>
2025-02-26 08:02:43 +0100

    Create PortableText.tsx

---
+++ components/PortableText.tsx
@@ -0,0 +1,34 @@
+import { PortableText, PortableTextComponents } from '@portabletext/react';
+import VimeoEmbed from './VimeoEmbed';
+
+interface VideoValue {
+  _type: 'video';
+  url: string;
+  caption?: string;
+  showThumbnail?: boolean;
+}
+
+const components: PortableTextComponents = {
+  types: {
+    video: ({ value }: { value: VideoValue }) => {
+      return (
+        <VimeoEmbed 
+          url={value.url} 
+          caption={value.caption} 
+          showThumbnail={value.showThumbnail} 
+        />
+      );
+    },
+    // your other custom components
+  },
+};
+
+interface PortableTextProps {
+  content: any;
+}
+
+const MyPortableText: React.FC<PortableTextProps> = ({ content }) => {
+  return <PortableText value={content} components={components} />;
+};
+
+export default MyPortableText;
