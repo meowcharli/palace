3336d7d48a052b3d45a6b51ed20fd356ae4f6f23
meowcharli <169078644+meowcharli@users.noreply.github.com>
2025-02-26 11:13:23 +0100

    Update layout.tsx

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -78,8 +78,8 @@ export default async function RootLayout({
                   value={footer as PortableTextBlock[]}
                 />
               ) : (
-                <div className="flex flex-col items-center py-14 lg:flex-row">
-                  <h3 className="mb-7 text-[#ddcdbf] text-center text-4xl leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
+                <div className="flex flex-col items-center py-19 lg:flex-row">
+                  <h3 className="mb-4 text-[#ddcdbf] text-center text-4xl leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
                     Designed with ♥️ in Serbia.
                   </h3>
                   <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
