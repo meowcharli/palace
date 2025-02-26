35f918ec85d4bfd86ca2f9f1c6d6306355a167da
meowcharli <169078644+meowcharli@users.noreply.github.com>
2025-02-26 08:30:32 +0100

    Update queries.ts

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -9,6 +9,7 @@ const postFields = /* groq */ `
   "slug": slug.current,
   excerpt,
   coverImage,
+  videoEmbed,
   "date": coalesce(date, _updatedAt),
   "author": author->{"name": coalesce(name, "Anonymous"), picture},
 `;
