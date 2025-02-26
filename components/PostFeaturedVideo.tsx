9dbc07b328616401135fa3cd51fdfa6affb4ad3a
meowcharli <169078644+meowcharli@users.noreply.github.com>
2025-02-26 08:03:26 +0100

    Create PostFeaturedVideo.tsx

---
+++ components/PostFeaturedVideo.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import VimeoEmbed from './VimeoEmbed';
+
+interface PostFeaturedVideoProps {
+  videoUrl: string;
+  title?: string;
+}
+
+const PostFeaturedVideo: React.FC<PostFeaturedVideoProps> = ({ videoUrl, title }) => {
+  if (!videoUrl) return null;
+  
+  return (
+    <div className="post-featured-video">
+      <VimeoEmbed 
+        url={videoUrl} 
+        caption={title} 
+        showThumbnail={true} 
+        className="mb-8" 
+      />
+    </div>
+  );
+};
+
+export default PostFeaturedVideo;
