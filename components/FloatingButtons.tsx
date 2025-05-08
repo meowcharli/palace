42bcc1c1389519aac1188002e4ba3344f897a636
meowcharli <169078644+meowcharli@users.noreply.github.com>
2025-05-08 22:23:54 +0200

    Update FloatingButtons.tsx

--- components/FloatingButtons.tsx
+++ components/FloatingButtons.tsx
@@ -124,25 +124,34 @@ export default function FloatingButtons({ isDraftMode = false }: FloatingButtons
               <div className="logo-hover-container">
                 {/* Default Logo */}
                 <svg 
-                  width="120" 
-                  height="60" 
-                  viewBox="0 0 392.9 181.59" 
+                  width="200" 
+                  height="30" 
+                  viewBox="0 0 755.08 100.86" 
                   xmlns="http://www.w3.org/2000/svg" 
                   className="logo-default"
                 >
-                  <path fill="#919191" d="M87.59 0v17.52H52.55v70.07H35.03V17.52H0V0zm93.84 0v26.15l-35.04 35.04v26.4h-17.52v-26.4L93.83 26.15V0h17.52v21.27l26.28 26.28 26.28-26.28V0zm76.33 0 17.52 17.52v17.52l-17.52 17.52h-52.55V87.6h-17.52V0zm-52.55 35.04h52.55V17.52h-52.55zM369.12 0v17.52h-70.07v17.52h52.55v17.52h-52.55v17.52h70.07V87.6h-87.59V0zM87.59 94v17.52H52.55v70.07H35.03v-70.07H0V94zm76.32 0 17.52 17.52v70.07h-17.52v-35.04h-52.55v35.04H93.84v-70.07L111.36 94zm-52.55 35.04h52.55v-17.52h-52.55zM275.28 94v17.52h-10.01l-26.28 26.28 26.28 26.28h10.01v17.52h-37.79v-16.02h6.88l-12.89-12.89-28.9 28.9h-14.89v-17.52h10.01l26.28-26.28-26.28-26.28h-10.01V93.99h37.79v16.02h-6.88l12.89 12.89 28.9-28.9z" />
-                  <path fill="#919191" d="M392.9 0v146.55h-92.16l35.03 35.04h-20.89l-43.8-43.8L314.88 94h20.89l-35.03 35.04h74.64V0z" />
+                  <g>
+                    <path className="cls-1" d="M205.78 9.48v16.38h-32.76v65.52h-16.38V25.86h-32.76V9.48h81.91Zm87.76 0v24.45l-32.76 32.76v24.69H244.4V66.69l-32.76-32.76V9.48h16.38v19.89l24.57 24.57 24.57-24.57V9.48zm71.37 0 16.38 16.38v16.38l-16.38 16.38h-49.14v32.76h-16.38V9.48zm-49.14 32.76h49.14V25.86h-49.14zM469.05 9.48v16.38h-65.52v16.38h49.14v16.38h-49.14V75h65.52v16.38h-81.9V9.48zm110.05 0v16.38h-32.76v65.52h-16.38V25.86H497.2V9.48h81.91Zm71.85 0 16.38 16.38v65.52h-16.38V58.62h-49.14v32.76h-16.38V25.86l16.38-16.38zm-49.14 32.76h49.14V25.86h-49.14zM755.08 9.48v16.38h-9.36l-24.57 24.57L745.72 75h9.36v16.38h-35.34V76.4h6.44l-12.05-12.05-27.03 27.03h-13.92V75h9.36l24.57-24.57-24.57-24.57h-9.36V9.48h35.34v14.98h-6.44l12.05 12.05 27.03-27.03z" fill="#919191"/>
+                    <rect className="cls-1" x="485.34" y="63.7" width="27.69" height="27.69" rx="13.84" ry="13.84" fill="#919191"/>
+                    <path className="cls-1" d="M72.27 0H28.58C12.79 0 0 12.8 0 28.59v15.85h12V28.59C12 19.44 19.44 12 28.58 12h43.69c9.15 0 16.59 7.44 16.59 16.59v43.69c0 9.14-7.44 16.58-16.59 16.58H28.58C19.44 88.86 12 81.42 12 72.28v-7.65H0v7.65c0 15.79 12.79 28.58 28.58 28.58h43.69c15.79 0 28.59-12.79 28.59-28.58V28.59C100.86 12.8 88.06 0 72.27 0" fill="#919191"/>
+                    <path className="cls-1" d="m83.05 50.43-32.1 32.1H33.17l25.67-25.68H0V44.02h58.84L33.17 18.34h17.78z" fill="#919191"/>
+                  </g>
                 </svg>
                 
                 {/* Hover Logo */}
                 <svg 
-                  width="120" 
-                  height="60" 
-                  viewBox="0 0 392.89 181.59" 
+                  width="200" 
+                  height="30" 
+                  viewBox="0 0 755.09 100.86" 
                   xmlns="http://www.w3.org/2000/svg" 
                   className="logo-hover"
                 >
-                  <path fill="#2E2E30" d="M392.89 0v108.95H61.39l72.64 72.64H90.79L0 90.79 90.79 0h43.24L61.39 72.64h295.2V0z" />
+                  <g>
+                    <path className="cls-1" d="M205.79 9.48v16.38h-32.76v65.52h-16.38V25.86h-32.76V9.48h81.91Zm87.75 0v24.45l-32.76 32.76v24.69H244.4V66.69l-32.76-32.76V9.48h16.38v19.89l24.57 24.57 24.57-24.57V9.48zm71.38 0 16.38 16.38v16.38l-16.38 16.38h-49.14v32.76H299.4V9.48zm-49.15 32.76h49.14V25.86h-49.14zM469.05 9.48v16.38h-65.52v16.38h49.14v16.38h-49.14V75h65.52v16.38h-81.9V9.48zm110.05 0v16.38h-32.76v65.52h-16.38V25.86H497.2V9.48h81.91Zm71.85 0 16.38 16.38v65.52h-16.38V58.62h-49.14v32.76h-16.38V25.86l16.38-16.38zm-49.14 32.76h49.14V25.86h-49.14zM755.09 9.48v16.38h-9.36l-24.57 24.57L745.73 75h9.36v16.38h-35.34V76.4h6.44l-12.05-12.05-27.03 27.03h-13.92V75h9.36l24.57-24.57-24.57-24.57h-9.36V9.48h35.34v14.98h-6.44l12.05 12.05 27.03-27.03z" fill="#404eff"/>
+                    <rect className="cls-1" x="485.34" y="63.7" width="27.69" height="27.69" rx="13.84" ry="13.84" fill="#404eff"/>
+                    <path className="cls-1" d="M72.28 12c9.14 0 16.58 7.44 16.58 16.58v43.69c0 9.14-7.44 16.58-16.58 16.58h-43.7C19.44 88.85 12 81.41 12 72.27V28.58C12 19.44 19.44 12 28.58 12h43.69m.01-12h-43.7C12.8 0 0 12.8 0 28.58v43.69c0 15.79 12.8 28.58 28.58 28.58h43.69c15.79 0 28.58-12.8 28.58-28.58V28.58C100.85 12.79 88.05 0 72.27 0Z" fill="#404eff"/>
+                    <path className="cls-1" d="M80.22 38.17v39.98H60.36V49.57H40.5v28.58H20.64V38.17l29.79-19.51z" fill="#404eff"/>
+                  </g>
                 </svg>
               </div>
             </Link>
@@ -172,7 +181,7 @@ export default function FloatingButtons({ isDraftMode = false }: FloatingButtons
                   data-name="Layer 2" 
                   xmlns="http://www.w3.org/2000/svg" 
                   viewBox="0 0 207.2 31.37" 
-                  height="20"
+                  height="18" 
                   className="svg-clickable"
                   onClick={toggleSearch}
                   aria-label="Search"
@@ -189,7 +198,7 @@ export default function FloatingButtons({ isDraftMode = false }: FloatingButtons
                     data-name="Layer 2" 
                     xmlns="http://www.w3.org/2000/svg" 
                     viewBox="0 0 274.11 31.37" 
-                    height="20"
+                    height="18"
                     className="svg-clickable"
                     aria-label="Contact"
                   >
@@ -213,7 +222,7 @@ export default function FloatingButtons({ isDraftMode = false }: FloatingButtons
         
         .logo-default {
           display: block;
-          transition: opacity 0.1s ease;
+          transition: opacity 0.3s ease;
         }
         
         .logo-hover {
@@ -221,7 +230,7 @@ export default function FloatingButtons({ isDraftMode = false }: FloatingButtons
           top: 0;
           left: 0;
           opacity: 0;
-          transition: opacity 0.1s ease;
+          transition: opacity 0.3s ease;
         }
         
         .logo-hover-container:hover .logo-default {
